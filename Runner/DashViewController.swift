713bd186e2c7a6329da93f02d43d7225f8558727
bodang <bodang@qq.com>
2017-01-20 04:17:08 +0000

    map view and corelocation worked

--- Runner/DashViewController.swift
+++ Runner/DashViewController.swift
@@ -9,16 +9,88 @@
 import UIKit
 import MapKit
 
-class DashViewController: UIViewController {
+class DashViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var MapView: MKMapView!
+    var runningBrain = RunningBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Location Delegate
+        runningBrain.getLocationManager().delegate = self
+        runningBrain.getLocationManager().desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        runningBrain.getLocationManager().requestAlwaysAuthorization()
+        
+        MapView.mapType = .standard
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations.last!
+        if currentLocation.horizontalAccuracy < 0 || currentLocation.horizontalAccuracy>100 {
+            return
+        }
+        let region = MKCoordinateRegionMakeWithDistance(currentLocation.coordinate, 1000, 1000)
+        MapView.setRegion(region, animated: true)
+        
+//        let paceInMinute = runningBrain.calculatePaceRate(Velocity: currentLocation.speed)["minute"]!
+//        let paceInSecond = runningBrain.calculatePaceRate(Velocity: currentLocation.speed)["second"]!
+//        switch algorithmUse {
+//        case 0:
+//            velocityUse = runningBrain.smoothingVelocityWithAverageNumber(Velocity: currentLocation.speed, Accuracy: currentLocation.horizontalAccuracy)
+//        case 1:
+//            velocityUse = runningBrain.smoothingVelocityWithThreeGapNumber(Velocity: currentLocation.speed, Accuracy: currentLocation.horizontalAccuracy)
+//        case 2:
+//            velocityUse = runningBrain.smoothingVelocityWithStanderDeviation(Velocity: currentLocation.speed, Accuracy: currentLocation.horizontalAccuracy)
+//        case 3:
+//            velocityUse = runningBrain.smoothingVelocityWithKalmanFiltering(Velocity: currentLocation.speed, Accuracy: currentLocation.horizontalAccuracy)
+//        default:
+//            break
+//        }
+//        let paceSmoothInMinute = runningBrain.calculatePaceRate(Velocity: velocityUse)["minute"]!
+//        let paceSmoothInSecond = runningBrain.calculatePaceRate(Velocity: velocityUse)["second"]!
+//        let paceInMinuteNorm = String(format:"%02d",paceInMinute)
+//        let paceInSecondNorm = String(format:"%02d",paceInSecond)
+//        let paceSmoothInMinuteNorm = String(format:"%02d",paceSmoothInMinute)
+//        let paceSmoothInSecondNorm = String(format:"%02d",paceSmoothInSecond)
+//        
+//        longitudeLbl.text = "Longitude: \(currentLocation.coordinate.longitude)"
+//        latitudeLbl.text = "Latitude: \(currentLocation.coordinate.latitude)"
+//        altitudeLbl.text = "Altitude: \(currentLocation.altitude)"
+//        horizontalAccuracyLbl.text = "Horizontal accuracy: \(currentLocation.horizontalAccuracy)"
+//        verticalAccuracyLbl.text = "Vertical accuracy: \(currentLocation.verticalAccuracy)"
+//        headingLbl.text = "Heading: \(currentLocation.course)"
+//        velocityLbl.text = "Velocity: \(velocityUse)"
+//        paceSmoothLbl.text = "PaceSmooth: \(paceSmoothInMinuteNorm):\(paceSmoothInSecondNorm)"
+//        paceLbl.text = "Pace: \(paceInMinuteNorm):\(paceInSecondNorm)"
+//        saveTestDataToDatabase(Velocity: currentLocation.speed, Accuracy: currentLocation.horizontalAccuracy)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let errorType = error.localizedDescription
+        let alertController = UIAlertController(title: "Location Service Error", message: errorType, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: { action in })
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+            
+        //need change!!!!!!!!!!!!!
+            
+        case .authorizedAlways, .authorizedWhenInUse:
+            runningBrain.getLocationManager().startUpdatingLocation()
+            MapView.showsUserLocation = true
+        default:
+            runningBrain.getLocationManager().stopUpdatingLocation()
+            MapView.showsUserLocation = false
+        }
     }
 
 
---
+++ Runner/RunningBrain.swift
@@ -0,0 +1,141 @@
+//
+//  RunnningBrain.swift
+//  Runner
+//
+//  Created by Bodang on 20/01/2017.
+//  Copyright © 2017 Bodang. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import CoreMotion
+
+class RunningBrain {
+    private let locationManager: CLLocationManager = CLLocationManager()
+//    private let motionManager: CMMotionManager = CMMotionManager()
+    
+    private var kalmanSwitch = 0
+    private var velocityAssetsIndex = 0
+    private var perviousVelocityAssetsIndex = 0
+    private var velocityAssets: Array<Double> = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    private var previousVelocityAssets: Array<Double> = [2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]
+    private var holdVelocity = 0.0
+    private var holdAccuracy = 0.0
+    func smoothingVelocityWithAverageNumber(Velocity:Double, Accuracy: Double) -> Double {
+        velocityAssets[velocityAssetsIndex % 10] = Velocity
+        velocityAssetsIndex += 1
+        return (velocityAssets[0] + velocityAssets[1] + velocityAssets[2] + velocityAssets[3] + velocityAssets[4] + velocityAssets[5] + velocityAssets[6] + velocityAssets[7] + velocityAssets[8] + velocityAssets[9]) / 10
+    }
+    
+    func smoothingVelocityWithThreeGapNumber(Velocity:Double, Accuracy: Double) -> Double {
+        velocityAssets[velocityAssetsIndex % 10] = Velocity
+        velocityAssetsIndex += 1
+        return (velocityAssets[0] + velocityAssets[4] + velocityAssets[9]) / 3
+    }
+    
+    func smoothingVelocityWithStanderDeviation(Velocity:Double, Accuracy: Double) -> Double {
+        velocityAssets[velocityAssetsIndex % 10] = Velocity
+        velocityAssetsIndex += 1
+        var temp = 0.0
+        let mean = (velocityAssets[0] + velocityAssets[1] + velocityAssets[2] + velocityAssets[3] + velocityAssets[4] + velocityAssets[5] + velocityAssets[6] + velocityAssets[7] + velocityAssets[8] + velocityAssets[9]) / 10
+        for value in velocityAssets {
+            temp += pow((value - mean), 2)
+        }
+        temp = sqrt(temp)
+        temp = temp / 3 / sqrt(10)
+        let result = velocityAssets.reduce( ([],[]), {
+            (a:([Double],[Double]),n:Double) -> ([Double],[Double]) in
+            (n>(mean-temp)&&n<(mean+temp)) ? (a.0+[n],a.1) : (a.0,a.1+[n])
+        })
+        if result.0.count == 0{
+            return holdVelocity
+        } else {
+            temp = 0
+            var i = 0
+            for value in result.0 {
+                i += 1
+                temp += value
+            }
+            holdVelocity = temp / Double(i)
+            return holdVelocity
+        }
+    }
+    
+    func smoothingVelocityWithKalmanFiltering(Velocity:Double, Accuracy: Double) -> Double {
+        velocityAssets[velocityAssetsIndex % 10] = Velocity
+        velocityAssetsIndex += 1
+        let mean = (velocityAssets[0] + velocityAssets[1] + velocityAssets[2] + velocityAssets[3] + velocityAssets[4] + velocityAssets[5] + velocityAssets[6] + velocityAssets[7] + velocityAssets[8] + velocityAssets[9]) / 10
+        let previousMeanVelocity = getPreviousVelocity()
+        let previousDeviation = getPreviousDeviation()
+        if(previousMeanVelocity < mean && Accuracy > 0) {
+            holdVelocity = previousMeanVelocity + sqrt(pow(previousDeviation,2)/(pow(previousDeviation,2) + pow(mean - holdVelocity,-2))) * (mean - holdVelocity)
+        } else if(previousMeanVelocity >= mean && Accuracy > 0){
+            holdVelocity = previousMeanVelocity - sqrt(pow(previousDeviation,2)/(pow(previousDeviation,2) + pow(mean - holdVelocity,-2))) * (holdVelocity - mean)
+        }
+        if kalmanSwitch < 20 {
+            kalmanSwitch += 1
+            setPerviousVelocity(velocity: mean)
+        } else if velocityChanging() {
+            if speeding() {
+                for _ in 1...10 {
+                    setPerviousVelocity(velocity: mean)
+                }
+            } else {
+                setPerviousVelocity(velocity: mean)
+            }
+        } else {
+            setPerviousVelocity(velocity: holdVelocity)
+            print("kalman")
+        }
+        holdAccuracy = Accuracy
+        return holdVelocity
+    }
+    
+    func calculatePaceRate(Velocity: Double) -> Dictionary<String, Int>{
+        let pace: Double
+        if Velocity < 0.1 {
+            pace = 0
+        } else {
+            pace = 1000 / Velocity
+        }
+        let paceInMinute: Int = Int(pace) / 60
+        let paceInSecond: Int = Int(pace) % 60
+        let paceRate: Dictionary<String, Int> = [
+            "minute": paceInMinute,
+            "second": paceInSecond
+        ]
+        return paceRate
+    }
+    
+    public func getLocationManager() -> CLLocationManager {
+        return locationManager
+    }
+    
+    public func setPerviousVelocity(velocity: Double) {
+        previousVelocityAssets[perviousVelocityAssetsIndex % 10] = velocity
+        perviousVelocityAssetsIndex += 1
+    }
+    
+    public func getPreviousVelocity() -> Double {
+        return (previousVelocityAssets[0] + previousVelocityAssets[1] + previousVelocityAssets[2] + previousVelocityAssets[3] + previousVelocityAssets[4] + previousVelocityAssets[5] + previousVelocityAssets[6] + previousVelocityAssets[7] + previousVelocityAssets[8] + previousVelocityAssets[9]) / 10
+    }
+    
+    public func getPreviousDeviation() -> Double {
+        var temp = 0.0
+        for value in velocityAssets {
+            temp += pow((value - getPreviousVelocity()), 2)
+        }
+        temp = sqrt(temp)
+        temp = temp / 3 / sqrt(10)
+        return temp
+    }
+    
+    public func velocityChanging() -> Bool {
+        return abs((velocityAssets[0] + velocityAssets[1] + velocityAssets[2] / 3) - (velocityAssets[7] + velocityAssets[8] + velocityAssets[9] / 3)) > 0.4
+    }
+    
+    public func speeding() -> Bool {
+        return abs((velocityAssets[0] + velocityAssets[1] + velocityAssets[2] / 3) - (velocityAssets[7] + velocityAssets[8] + velocityAssets[9] / 3)) > 1.5
+    }
+    
+}
